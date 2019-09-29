3a23f6fe0c15b98158a8fa5e128e57b891e8c1d8
Kim Hamper <kim.hamper@gmail.com>
2019-09-29 14:11:10 -0400

    Clean up spacing

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -13,26 +13,34 @@ function Toggle({ name, onChange, option1, option2, value }) {
 
   return (
     <>
-      <label className="radio">
-        <input
-          checked={value === option1}
-          id={option1}
-          name={name}
-          onChange={handleChange}
-          type="radio"
-          value={option1}
-        />
+      <input
+        checked={value === option1}
+        id={option1}
+        name={name}
+        onChange={handleChange}
+        type="radio"
+        value={option1}
+      />
+      <label
+        className="radio"
+        htmlFor={option1}
+        style={{ paddingLeft: "0.5rem", paddingRight: "1rem" }}
+      >
         {option1}
       </label>
-      <label className="radio">
-        <input
-          checked={value === option2}
-          id={option2}
-          name={name}
-          onChange={handleChange}
-          type="radio"
-          value={option2}
-        />
+      <input
+        checked={value === option2}
+        id={option2}
+        name={name}
+        onChange={handleChange}
+        type="radio"
+        value={option2}
+      />
+      <label
+        className="radio"
+        htmlFor={option2}
+        style={{ paddingLeft: "0.5rem" }}
+      >
         {option2}
       </label>
     </>
